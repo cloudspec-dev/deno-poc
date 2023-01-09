c6078b975299e5d6c8a00e591c5e3a9687e512a0
Sebastian Korfmann <sebastian@korfmann.net>
2023-01-09 17:52:31 +0100

    More context

--- examples/node/original-construct.ts
+++ examples/node/original-construct.ts
@@ -1,4 +1,5 @@
 // A stack that would be part of a default aws-cdk app based on classic Node / Typescript and doesn't know anything about Deno
+// The import_map.json file maps this into Deno land and could be auto generated based on package.json
 
 import { Construct } from 'constructs'
 import { Bucket } from 'aws-cdk-lib/aws-s3'
